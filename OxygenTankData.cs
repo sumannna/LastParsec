42ab60c368a8ef2af4b6be1719bdcd0477e365c5
sumannna <79308736+sumannna@users.noreply.github.com>
2026-03-31 21:59:10 +0900

    Delete OxygenTankData.cs

--- OxygenTankData.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "NewOxygenTank", menuName = "Inventory/OxygenTankData")]
-public class OxygenTankData : ItemData
-{
-    [Header("タンク設定")]
-    public float maxTankOxygen = 100f;
-}
