8aaca3b4cf7a0173f65278106365cbe59b07573b
sumannna <79308736+sumannna@users.noreply.github.com>
2026-03-31 22:02:15 +0900

    Delete ThrusterTankData.cs

--- ThrusterTankData.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "NewThrusterTank", menuName = "Inventory/ThrusterTankData")]
-public class ThrusterTankData : ItemData
-{
-    [Header("タンク設定")]
-    public float maxTankFuel = 100f;
-}
