21ca8d455923bb9ca10c1b60fe08ee8e962277ef
sumannna <79308736+sumannna@users.noreply.github.com>
2026-04-14 13:44:01 +0900

    Delete Equipment directory

--- Equipment/SpacesuitData.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "NewSpacesuit", menuName = "Inventory/SpacesuitData")]
-public class SpacesuitData : ItemData
-{
-    [Header("宇宙服設定")]
-    public float maxDurability = 100f;
-    public float damageReduction = 0.5f; // ダメージ軽減率（0〜1）
-}
