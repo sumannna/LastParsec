19ae17effa8781919088229fa9f3c99db878183a
sumannna <79308736+sumannna@users.noreply.github.com>
2026-04-14 13:43:24 +0900

    Delete Chest directory

--- Chest/ChestData.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "NewChestData", menuName = "LastParsec/ChestData")]
-public class ChestData : PlaceableData
-{
-    [Header("チェスト設定")]
-    public int slotCount = 10;
-}
